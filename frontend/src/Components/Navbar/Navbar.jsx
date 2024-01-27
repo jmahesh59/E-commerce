eb6896f87bf626a7402258b50b8a2a0ca67df843
mahesh <jadhao.mahesh1009@gmail.com>
2024-01-27 12:39:11 +0530

    Link tag added to the navbar menu

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import  logo from "../Assets/logo.png"
 import cart_icon from "../Assets/cart_icon.png"
+import { Link } from 'react-router-dom'
 function Navbar() {
     const [menu ,setMenu] = useState("shop")
   return (
@@ -11,14 +12,14 @@ function Navbar() {
             <p>SHOPPER</p>
         </div>
         <ul className='nav-menu'>
-            <li onClick={(e)=>{setMenu(e.target.id)}} id='shop'>Shop {menu==="shop"?(<hr/>):(<></>)}</li>
-            <li onClick={(e)=>{setMenu(e.target.id)}} id='men'>Men {menu==="men"?(<hr/>):(<></>)}</li>
-            <li onClick={(e)=>{setMenu(e.target.id)}} id='women'>Women{menu==="women"?(<hr/>):(<></>)}</li>
-            <li onClick={(e)=>{setMenu(e.target.id)}} id='kids'>Kids {menu==="kids"?(<hr/>):(<></>)}</li>
+        <Link to={"/"}><li onClick={(e)=>{setMenu(e.target.id)}} id='shop'>Shop {menu==="shop"?(<hr/>):(<></>)}</li></Link> 
+        <Link to={"/mens"}>   <li onClick={(e)=>{setMenu(e.target.id)}} id='men'>Men {menu==="men"?(<hr/>):(<></>)}</li></Link> 
+        <Link to={"/womens"}> <li onClick={(e)=>{setMenu(e.target.id)}} id='women'>Women{menu==="women"?(<hr/>):(<></>)}</li></Link>    
+        <Link to={"/kids"}><li onClick={(e)=>{setMenu(e.target.id)}} id='kids'>Kids {menu==="kids"?(<hr/>):(<></>)}</li></Link>    
         </ul>
         <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart_icon} alt="cart" />
+         <Link to={"/login"}>  <button>Login</button></Link> 
+           <Link to={"/cart"}> <img src={cart_icon} alt="cart" /></Link>
             <div className="nav-cart-count">0</div>
         </div>
     </div>
