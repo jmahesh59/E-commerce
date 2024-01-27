6a96b6114d57550193807afba5e310f55eef5c4f
mahesh <jadhao.mahesh1009@gmail.com>
2024-01-27 12:22:45 +0530

    Browser Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import{BrowserRouter ,Routes,Route} from 'react-router-dom'
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSingup from './Pages/LoginSingup';
 function App() {
   return (
-    <div >
+    <div>
+    <BrowserRouter >
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path="/mens" element={<ShopCategory category="men"/>}/>
+        <Route path="/womens" element={<ShopCategory category="women"/>}/>
+        <Route path="/kids" element={<ShopCategory category="kid"/>}/>
+        <Route path='/product' element={<Product/>}>
+           <Route path=":productId" element={<Product/>}/>
+        </Route>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<LoginSingup/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
