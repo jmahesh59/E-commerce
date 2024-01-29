4fc0919551d1a2ea2468dad7addc24dd5d1a26fc
mahesh <jadhao.mahesh1009@gmail.com>
2024-01-29 12:08:47 +0530

    login html css

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -12,13 +12,13 @@ function Navbar() {
             <p>SHOPPER</p>
         </div>
         <ul className='nav-menu'>
-        <Link to={"/"}><li onClick={(e)=>{setMenu(e.target.id)}} id='shop'>Shop {menu==="shop"?(<hr/>):(<></>)}</li></Link> 
-        <Link to={"/mens"}>   <li onClick={(e)=>{setMenu(e.target.id)}} id='men'>Men {menu==="men"?(<hr/>):(<></>)}</li></Link> 
-        <Link to={"/womens"}> <li onClick={(e)=>{setMenu(e.target.id)}} id='women'>Women{menu==="women"?(<hr/>):(<></>)}</li></Link>    
-        <Link to={"/kids"}><li onClick={(e)=>{setMenu(e.target.id)}} id='kids'>Kids {menu==="kids"?(<hr/>):(<></>)}</li></Link>    
+        <Link to={"/"}><li onClick={(e)=>{setMenu('shop')}} id='shop'>Shop {menu==="shop"?(<hr/>):(<></>)}</li></Link> 
+        <Link to={"/mens"}>   <li onClick={(e)=>{setMenu('men')}} id='men'>Men {menu==="men"?(<hr/>):(<></>)}</li></Link> 
+        <Link to={"/womens"}> <li onClick={(e)=>{setMenu('women')}} id='women'>Women{menu==="women"?(<hr/>):(<></>)}</li></Link>    
+        <Link to={"/kids"}><li onClick={(e)=>{setMenu('kids')}} id='kids'>Kids {menu==="kids"?(<hr/>):(<></>)}</li></Link>    
         </ul>
         <div className="nav-login-cart">
-         <Link to={"/login"}>  <button>Login</button></Link> 
+         <Link to={"/login"}>  <button onClick={(e)=>{setMenu('login')}}>Login </button></Link> 
            <Link to={"/cart"}> <img src={cart_icon} alt="cart" /></Link>
             <div className="nav-cart-count">0</div>
         </div>
